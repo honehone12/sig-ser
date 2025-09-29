236aee75bf427cc08305a5e27f7ba15cee29c700
Twelfthonehone <honehone.41games@gmail.com>
2025-09-30 01:59:06 +0900

    added json impl

---
+++ json/json.go
@@ -0,0 +1,73 @@
+package json
+
+import (
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/json"
+	"sigser"
+	"time"
+)
+
+type SigSerJson struct {
+	inner sigser.SigSer
+}
+
+type SigDeJson struct {
+	inner sigser.SigDe
+}
+
+func (ser *SigSerJson) Marshal(v any) ([]byte, error) {
+	inner, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now().Unix()
+
+	b := make([]byte, len(inner)+8)
+	binary.BigEndian.PutUint64(b, uint64(now))
+	copy(b[8:], inner)
+
+	sig, err := ser.inner.Sign(b)
+	if err != nil {
+		return nil, err
+	}
+
+	sigP := sigser.SignedPayload{
+		Payload:   string(inner),
+		Signature: base64.StdEncoding.EncodeToString(sig),
+		Timestamp: uint64(now),
+	}
+
+	return json.Marshal(sigP)
+}
+
+func (de *SigDeJson) Unmarshal(data []byte, v any) error {
+	sigP := sigser.SignedPayload{}
+	err := json.Unmarshal(data, &sigP)
+	if err != nil {
+		return err
+	}
+
+	err = sigser.CheckTimestamp(sigP.Timestamp)
+	if err != nil {
+		return err
+	}
+
+	inner := []byte(sigP.Payload)
+	b := make([]byte, len(inner)+8)
+	binary.BigEndian.PutUint64(b, sigP.Timestamp)
+	copy(b[8:], inner)
+
+	sig, err := base64.StdEncoding.DecodeString(sigP.Signature)
+	if err != nil {
+		return err
+	}
+
+	err = de.inner.Verify(inner, sig)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(inner, v)
+}
